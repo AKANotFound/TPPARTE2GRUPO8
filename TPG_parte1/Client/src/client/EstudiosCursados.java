f803e7b3326d75242ee97f5d0032d331994bccc8
valentiaga <104161485+valentiaga@users.noreply.github.com>
2022-04-22 17:26:03 -0300

    Update EstudiosCursados.java

--- TPG_parte1/Client/src/client/EstudiosCursados.java
+++ TPG_parte1/Client/src/client/EstudiosCursados.java
@@ -1,4 +1,8 @@
 package client;
 
-public class EstudiosCursados {
+public abstract class EstudiosCursados extends Requisitos {
+	public abstract double comparaCon (EstudiosCursados estudios);
+	public abstract double comparaConPrimario();
+	public abstract double comparaConSecundario();
+	public abstract double comparaConTerciario();
 }
