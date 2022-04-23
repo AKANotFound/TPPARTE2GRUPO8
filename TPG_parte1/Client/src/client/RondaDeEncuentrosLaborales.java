f6cc0a855ef2a46b5f83227eb4a112a656871571
simof <simof@DESKTOP-FQ5CCOT.fibertel.com.ar>
2022-04-23 15:44:25 -0300

    probando

--- TPG_parte1/Client/src/client/RondaDeEncuentrosLaborales.java
+++ TPG_parte1/Client/src/client/RondaDeEncuentrosLaborales.java
@@ -6,7 +6,7 @@ public class RondaDeEncuentrosLaborales {
 	
 	private ArrayList <Persona_Empleador>empleadores =  Agencia.getInstancia().empleadores;
 	private ArrayList <Persona_EmpleadoPretenso> empleadosPretensos =  Agencia.getInstancia().empleadosPretensos;
-	
+	//probando
 	
 	public void generaListaAsignaciones ()
 	{
