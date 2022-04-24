fef9d22ba22461707fa97d17fc8efb256e602e47
simof <simof@DESKTOP-FQ5CCOT.fibertel.com.ar>
2022-04-24 20:17:49 -0300

    creacion excepciones(falta try y catch)

--- TPG_parte1/Client/src/excepciones/ListaNoGeneradaException.java
+++ TPG_parte1/Client/src/excepciones/ListaNoGeneradaException.java
@@ -1,6 +1,7 @@
 package excepciones;
 
 public class ListaNoGeneradaException extends Exception {
+	
 	public ListaNoGeneradaException()
 	{
 		super("la lista de asignaciones no ha sido generada");
