41176cfe1d353bd3a425048771e9157e989ba295
simof <simof@DESKTOP-FQ5CCOT.fibertel.com.ar>
2022-04-24 15:34:39 -0300

    creacion de paquete excepciones

---
+++ TPG_parte1/Client/src/excepciones/ErrorContrasenaException.java
@@ -0,0 +1,5 @@
+package excepciones;
+
+public class ErrorContrasenaException extends Exception {
+
+}
---
+++ TPG_parte1/Client/src/excepciones/ListaNoGeneradaException.java
@@ -0,0 +1,5 @@
+package excepciones;
+
+public class ListaNoGeneradaException extends Exception {
+
+}
