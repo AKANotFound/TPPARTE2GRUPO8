2f238c65929ad97d62a1c5e1148522496c3c77f5
simof <simof@DESKTOP-FQ5CCOT.fibertel.com.ar>
2022-04-24 16:38:31 -0300

    creacion de excepciones

--- TPG_parte1/Client/src/excepciones/ErrorContrasenaException.java
+++ TPG_parte1/Client/src/excepciones/ErrorContrasenaException.java
@@ -1,5 +1,13 @@
 package excepciones;
 
 public class ErrorContrasenaException extends Exception {
+	private String contrasenaIngresada;
+
+	public ErrorContrasenaException(String contrasenaIngresada) {
+		super();
+		this.contrasenaIngresada = contrasenaIngresada;
+	}
+	
+	
 
 }
--- TPG_parte1/Client/src/excepciones/ErrorUsuarioException.java
+++ TPG_parte1/Client/src/excepciones/ErrorUsuarioException.java
@@ -1,5 +1,14 @@
 package excepciones;
 
 public class ErrorUsuarioException extends Exception {
+	private String usuarioIngresado;
+
+	public ErrorUsuarioException(String usuarioIngresado) {
+		super();
+		this.usuarioIngresado = usuarioIngresado;
+	}
+	
+	
+	
 
 }
