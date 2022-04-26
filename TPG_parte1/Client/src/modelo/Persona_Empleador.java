bf2e2c0fe4ac55298285744c48db79ba77f7ca0b
marti <marti@192.168.0.218>
2022-04-26 18:02:28 -0300

    alv el decorator

--- TPG_parte1/Client/src/modelo/Persona_Empleador.java
+++ TPG_parte1/Client/src/modelo/Persona_Empleador.java
@@ -2,18 +2,27 @@ package modelo;
 
 import java.util.ArrayList;
 
-public class Persona_Empleador extends Persona {
+public abstract class Persona_Empleador extends Persona {
 
 	private String razonSocial;
-    private boolean personaFisica;
     private Rubro rubro;
     private double puntajeAspectos[]=new double[7];
     private ArrayList<PersonaAsignada> elegidos = new ArrayList<PersonaAsignada>();
+    private double sueldoOfrecido;
     
+    public Persona_Empleador(Cuenta cuenta) 
+    {
+    	super(cuenta);
+    }
     
-    public Persona_Empleador(Cuenta cuenta) {
+    public Persona_Empleador(Cuenta cuenta, String razonSocial, Rubro rubro, double sueldoOfrecido) 
+    {
 		super(cuenta);
+		this.razonSocial = razonSocial;
+		this.rubro = rubro;
+		this.sueldoOfrecido = sueldoOfrecido;
 	}
+
 
     @Override
 	public void eleccion() {
@@ -23,9 +32,16 @@ public class Persona_Empleador extends Persona {
 	public double[] getPuntajeAspectos() {
 		return puntajeAspectos;
 	}
+	
+	
+
+	public double getSueldoOfrecido() {
+		return sueldoOfrecido;
+	}
 
 	public void agregaElegidos (PersonaAsignada personaAsignada)
 	{
 		this.elegidos.add(personaAsignada);
 	}
+
 }
