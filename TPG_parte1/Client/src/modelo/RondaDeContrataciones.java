0316c552690595311b4feece3ed5b5962600fe84
valentiaga <104161485+valentiaga@users.noreply.github.com>
2022-04-26 21:36:13 -0300

    Update RondaDeContrataciones.java

--- TPG_parte1/Client/src/modelo/RondaDeContrataciones.java
+++ TPG_parte1/Client/src/modelo/RondaDeContrataciones.java
@@ -1,5 +1,26 @@
 package modelo;
 
 public class RondaDeContrataciones {
-
+	
+	public void calculoCoincidencias ()
+	{
+		ArrayList <Persona_Empleador> empleadores = Agencia.getInstancia().getEmpleadores();
+		for (int i= 0; i< empleadores.size(); i++)
+		{
+			ArrayList<PersonaAsignada> elegidos = empleadores.get(i).getElegidos();
+			Persona_Empleador empleador = empleadores.get(i);
+			for (int j=0; j<elegidos.size();j++) 
+			{
+				Persona_EmpleadoPretenso empleadoElegido = (Persona_EmpleadoPretenso) elegidos.get(j).getPersona();
+				if (empleador.equals(empleadoElegido.getElegido().getPersona()))
+				{
+					Ticket_BusquedaEmpleado ticket = (Ticket_BusquedaEmpleado) empleador.getTicket();
+					ticket.setCantEmpleadosObtenidos(ticket.getCantEmpleadosObtenidos()+1);
+					empleadoElegido.getTicket().finalizar();
+				}
+			}
+		}
+	}
+	
 }
+
