feb5521ca43f0aade04b44db887a15535d355835
MartiPresa <102812269+MartiPresa@users.noreply.github.com>
2022-05-05 16:55:30 -0300

    Update Persona.java

--- TPG_parte1/Client/src/entidades/Persona.java
+++ TPG_parte1/Client/src/entidades/Persona.java
@@ -1,20 +1,38 @@
 package entidades;
 
+import sistema.FuncionalidadEmpleadoPretenso;
+import sistema.FuncionalidadPersona;
+
+
+/**
+ * @author grupoxd <br>
+ * Clase abstracta que representa una Persona, solo guarda informaci�n acerca de ella.
+ */
 
 public abstract class Persona extends Usuario {
-    protected Ticket ticket;
-    private double costoServicio;    
-    private ListaDeAsignacion listaDeAsignacion;
-    private boolean eligio;
-    private int puntaje;
-    
-    public Persona(Cuenta cuenta) {
+	protected Ticket ticket;
+	private double costoServicio;
+	private ListaDeAsignacion listaDeAsignacion;
+	private boolean eligio;
+	private int puntaje;
+	//protected FuncionalidadPersona funcionalidad = null;
+ 
+	/**
+	 * Constructor con 1 parametro para setear la cuenta de la persona. Al resto de los atributos se los inicializa en el mismo constructor
+	 * <br>
+	 * @param cuenta : parametro de tipo String que representa la cuenta de la Persona
+	 */
+	
+	public Persona(Cuenta cuenta) {
 		super(cuenta);
 		this.ticket = null;
 		this.costoServicio = 0;
-		this.eligio=false;
+		this.eligio = false;
 		this.puntaje = 0;
+		this.listaDeAsignacion = null;
 	}
+  
+	
 
 	public Ticket getTicket() {
 		return ticket;
@@ -25,12 +43,12 @@ public abstract class Persona extends Usuario {
 	}
 
 	public double getRemuneracion() {
-		if(this.ticket == null)
+		if (this.ticket == null)
 			return 0;
 		else
 			return this.ticket.getFormularioDeBusqueda().getRemuneracion().getRemuneracion();
 	}
-	
+
 	public ListaDeAsignacion getListaDeAsignacion() {
 		return listaDeAsignacion;
 	}
@@ -42,11 +60,11 @@ public abstract class Persona extends Usuario {
 	public boolean isEligio() {
 		return eligio;
 	}
-
+ 
 	public void setEligio(boolean eligio) {
 		this.eligio = eligio;
 	}
-	
+
 	public int getPuntaje() {
 		return puntaje;
 	}
@@ -54,7 +72,7 @@ public abstract class Persona extends Usuario {
 	public void setPuntaje(int puntaje) {
 		this.puntaje = puntaje;
 	}
-	
+
 	public double getCostoServicio() {
 		return costoServicio;
 	}
@@ -62,5 +80,10 @@ public abstract class Persona extends Usuario {
 	public void setCostoServicio(double costoServicio) {
 		this.costoServicio = costoServicio;
 	}
-}
 
+	@Override
+	public String toString() {
+		return ", eligio=" + eligio + ", puntaje=" + puntaje + "]";
+	}
+
+}
