333264b633c41c8b25d19dc976a23657aef334f5
valentiaga <104161485+valentiaga@users.noreply.github.com>
2022-05-05 17:00:58 -0300

    Update Locacion_Presencial.java

--- TPG_parte1/Client/src/tablas/Locacion_Presencial.java
+++ TPG_parte1/Client/src/tablas/Locacion_Presencial.java
@@ -21,4 +21,9 @@ public class Locacion_Presencial implements ILocacion {
 	public double comparaConIndistinto() {
 		return -1;
 	}
+	
+	@Override
+	public String toString() {
+		return " presencial";
+	}
 }
