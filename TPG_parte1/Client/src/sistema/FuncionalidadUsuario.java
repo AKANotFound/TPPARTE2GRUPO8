8ef69fef94f8eb0a36632b84c54ff2ca62bbe1f7
MartiPresa <102812269+MartiPresa@users.noreply.github.com>
2022-05-05 17:07:12 -0300

    Update FuncionalidadUsuario.java

--- TPG_parte1/Client/src/sistema/FuncionalidadUsuario.java
+++ TPG_parte1/Client/src/sistema/FuncionalidadUsuario.java
@@ -3,14 +3,19 @@ package sistema;
 import entidades.Usuario;
 
 public abstract class FuncionalidadUsuario {
-	protected Usuario usuario;
+	//protected Usuario usuario;
 	
+	/*public FuncionalidadUsuario() {
+		super();
+		
+	}
 	public FuncionalidadUsuario(Usuario usuario) {
 		super();
 		this.usuario = usuario;
-	}
+	} 
+
 
 	public Usuario getUsuario() {
 		return usuario;
-	}
+	}*/
 }
