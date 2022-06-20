bd5d0e87f835f2b5f4591a59ed876700a5751d9f
simof <simof@DESKTOP-FQ5CCOT.fibertel.com.ar>
2022-06-20 11:20:03 -0300

    añadi identificadores de vistas

--- GRUPO8/Client/src/vista/Ventana.java
+++ GRUPO8/Client/src/vista/Ventana.java
@@ -18,6 +18,14 @@ public class Ventana extends JFrame implements IVista {
 	
 	public final static String VISTA_INICIAL = "VistaInicial";
 	public final static String VISTA_REGISTRAR_ADMINISTRADOR = "VistaRegistrarAdministrador";
+	public final static String VISTA_REGISTRAR_EMPLEADO_PRETENSO = "VistaRegistrarEmpleadoPretenso";
+	public final static String VISTA_REGISTRAR_EMPLEADOR = "VistaRegistrarEmpleador";
+	public final static String VISTA_FUNCIONALIDADES_ADMINISTRADOR = "VistaFuncionalidadesAdministrador";
+	public final static String VISTA_FUNCIONALIDADES_PERSONA = "VistaFuncionalidadesAdministrador";
+	public final static String VISTA_FORMULARIO_DE_BUSQUEDA_EMPLEADO_PRETENSO = "VistaFormularioDeBusquedaEmpleadoPretenso";
+	public final static String VISTA_FORMULARIO_DE_BUSQUEDA_EMPLEADOR = "VistaFormularioDeBusquedaEmpleador";
+	public final static String VISTA_GESTION_TICKET_PERSONA = "VistaGestionTicketPersona";
+	public final static String VISTA_SIMULACION = "VistaSimulacion";
 
 	public Ventana() {
 		vistaInicial = new VistaInicial();
