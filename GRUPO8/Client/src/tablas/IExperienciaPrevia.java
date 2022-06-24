240dcfa305f184405843a42ee8027f06a843d3a6
sanchezmilton <sanchezgonzalezmilton@gmail.com>
2022-06-24 20:54:59 -0300

    commit

--- GRUPO8/Client/src/tablas/IExperienciaPrevia.java
+++
@@ -1,9 +0,0 @@
-package tablas;
-
-public interface IExperienciaPrevia {
-	
-	public abstract double comparaCon (IExperienciaPrevia e);
-	public abstract double comparaConNada();
-	public abstract double comparaConMedia();
-	public abstract double comparaConMucha();
-}
